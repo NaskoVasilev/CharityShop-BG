52ee837dc298fda999f71ccc9043d9925177c24f
NaskoVasilev <nasko_vasilev@abv.bg>
2019-04-04 13:39:42 +0300

    Remove some data

--- config/config.js
+++ config/config.js
@@ -4,18 +4,17 @@ module.exports = {
     environment: 'production',
     development: {
         url: 'http://localhost:8000',
-        // connectionString:'mongodb://nasko:nasko-js-24112001@ds253959.mlab.com:53959/charity-shop',
         connectionString: 'mongodb://localhost:27017/ShopDatabase',
         rootPath: path.normalize(path.join(__dirname, '../'))
     },
     production: {
         url: 'https://charity-shop-bg.herokuapp.com',
-        connectionString: 'mongodb://nasko:nasko-js-24112001@ds253959.mlab.com:53959/charity-shop',
+        connectionString: 'production database connection string comes here',
         rootPath: path.normalize(path.join(__dirname, '../'))
     },
     email: {
         username: 'charityShopAppJS@gmail.com',
-        password: 'charity-js-app-secret',
+        password: 'email password comes here',
         service: 'gmail'
     }
 }
